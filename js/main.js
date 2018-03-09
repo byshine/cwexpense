e0abe7c4b8ffa390a19591a7cad1c882196deb54
Aaron Ahn <aaron@byshine.net>
2018-03-08 23:14:07 -0800

    localstorage implenetation

--- js/main.js
+++ js/main.js
@@ -1,19 +1,48 @@
-function ExpenseModel() {
+function ExpenseModel( map ) {
 
-  //Encapsulated Map
-  const expenseMap = new Map();
+  //Private Variables
+  const expenseMap = loadFromCache();
+  const expenseObjectArray = JSON.parse(localStorage.getItem("expenseArray")) || [];
+
+  //Private Methods
+  function loadFromCache() {
+    if ( localStorage.length == 0 ) {
+      return new Map();
+    } else {
+
+      const expenseMap = new Map();
+      const expenseObjectArray = JSON.parse( localStorage.getItem("expenseArray") );
+      let description = null;
+      expenseObjectArray.forEach(function( expenseObject ) {
+        description = expenseObject.description;
+        if ( expenseMap.has( description ) ) {
+          expenseMap.get( description ).push( expenseObject );
+        } else {
+          const expenseArray = [];
+          expenseArray.push( expenseObject );
+          expenseMap.set( description, expenseArray );
+        }
+      })
+      return expenseMap;
+    }
+  }
 
   return {
 
+    //Public Methods
     add : function( inputObject, amount, date, description) {
       if ( inputObject && description && date && amount ) {
 
         const expenseObject = {
             dom: inputObject,
+            description: description,
             date: date,
             amount: amount,
             hidden: false
         };
+
+        expenseObjectArray.push(expenseObject);
+        localStorage.setItem("expenseArray", JSON.stringify(expenseObjectArray));
 
         if ( expenseMap.has( description ) ) {
           expenseMap.get( description ).push( expenseObject );
@@ -22,6 +51,8 @@ function ExpenseModel() {
           expenseArray.push( expenseObject );
           expenseMap.set( description, expenseArray );
         }
+
+
       }
     },
 
@@ -42,6 +73,27 @@ function ExpenseModel() {
 }
 
 function ExpenseController( model ) {
+
+  //Prviate Methods
+  function renderFromCache( model ) {
+    const map = model.getMap();
+    if ( map.size != 0 ) {
+
+      map.forEach(function(value, key) {
+        value.forEach(function ( expenseObject ) {
+          const inputObject = $("#ec-activitiy-table-tbody").append(
+            "<tr>" +
+            "<td>" + expenseObject.description + "</td>" +
+            "<td>" + expenseObject.date + "</td>" +
+            "<td>" + expenseObject.amount.toFixed(2) + "</td>" +
+            "</tr>"
+          );          
+        });
+      });
+    }
+  }
+
+  renderFromCache( model );
 
   return {
 
@@ -81,10 +133,17 @@ function ExpenseController( model ) {
   }
 }
 
+
+
 const model = new ExpenseModel();
 const controller = new ExpenseController( model );
+controller.renderTotal();
+//localStorage.clear();
+console.log(localStorage);
+
 
 $("#ec-summary-submitbutton").click( function( e ) {
+  e.preventDefault();
   const inputArray = controller.parseInputs();
   if ( inputArray ) {
     controller.addDataRow( inputArray );
